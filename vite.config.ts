e558709642da1ffeae993078dd6107d5894a57e4
eltigerchino <chewteeming01@gmail.com>
2024-05-07 01:38:52 +0800

    fix vite re-optimizing frequently

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	optimizeDeps: {
+		entries: ['src/routes/**/+*.{svelte,js,ts}']
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
